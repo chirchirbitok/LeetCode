707494889c14e1e6b3abd65e15146fe2b20c911d
chirchirbitok <douglasbitok@gmail.com>
2022-07-24 22:55:29 +0300

    Moreof it

---
+++ safcom.js
@@ -0,0 +1,10 @@
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let arrSize = A.length;
+    for(let i =0; i<arrSize; i++){
+        if(A[i] % 4 === 0 ){
+            let result = A[i]/4;
+            
+        }
+    }
+}
